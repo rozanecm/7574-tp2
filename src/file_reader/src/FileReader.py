5753754b1d5eebb7236f9e24e25d548fec44d01c
Matias Rozanec <rozanecm@gmail.com>
2020-11-16 09:29:46 -0300

    update file reader
    adapt method to read any file, thus enabling sending both files via rabbit.

--- src/file_reader/src/FileReader.py
+++ src/file_reader/src/FileReader.py
@@ -2,8 +2,6 @@ import json
 import logging
 import os
 import zipfile
-from time import sleep
-
 
 LINES_TO_SEND = 10000
 
@@ -16,37 +14,36 @@ class FileReader():
 
     def run(self):
         logging.info("file reader running...")
-        self.transmit_businesses_info()
-        self.transmit_reviews_info()
+
+        businesses_zip_path = os.path.join(os.getcwd(), "data_files", "yelp_academic_dataset_business.json.zip")
+        reviews_zip_path = os.path.join(os.getcwd(), "data_files", "yelp_academic_dataset_review.json.zip")
+
+        self.transmit_file_info(businesses_zip_path, "yelp_academic_dataset_business.json", "businesses")
+        self.transmit_file_info(reviews_zip_path, "yelp_academic_dataset_review.json", "reviews")
+
         self.connection.close()
 
-    def transmit_businesses_info(self):
-        # TODO implementar
-        logging.info(os.listdir(os.path.join(os.getcwd(), "data_files")))
+    def transmit_file_info(self, zip_path, file_in_zip, data_identifier):
         lines_read = 0
-        with zipfile.ZipFile(os.path.join(os.getcwd(), "data_files", "yelp_academic_dataset_business.json.zip")) as z:
-            with z.open("yelp_academic_dataset_business.json") as f:
+        with zipfile.ZipFile(zip_path) as z:
+            with z.open(file_in_zip) as f:
                 lines_to_send = []
                 for line in f:
                     lines_read += 1
                     lines_to_send.append(line.decode().strip())
                     if len(lines_to_send) == LINES_TO_SEND:
-                        self.send_businesses(lines_to_send)
+                        self.send_bulk(lines_to_send, data_identifier)
                         lines_to_send.clear()
                 # send remaining lines,
                 # because if line count is not multiple of 100,
                 # then the last bulk will stay unsent.
                 if len(lines_to_send):
-                    self.send_businesses(lines_to_send)
-        logging.info("Read {} lines from Businesses info.".format(lines_read))
+                    self.send_bulk(lines_to_send, data_identifier)
+        logging.info("Read {} lines from {} info.".format(lines_read, data_identifier))
 
-    def send_businesses(self, lines_to_send):
-        dict_to_send = {"businesses": lines_to_send}
+    def send_bulk(self, lines_to_send, data_identifier):
+        dict_to_send = {data_identifier: lines_to_send}
         body = json.dumps(dict_to_send)
         self.channel.basic_publish(exchange='',
                                    routing_key='raw_files',
                                    body=body)
-
-    def transmit_reviews_info(self):
-        # TODO implementar
-        pass
