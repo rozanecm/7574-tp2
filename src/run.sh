ac53b934881fbbb7b108eb0eb365689a63949e01
Matias Rozanec <rozanecm@gmail.com>
2020-11-20 16:33:00 -0300

    update run.sh script
    add option to start many workers.

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# docker-compose -f docker-compose-dev.yaml up
-docker-compose -f docker-compose-dev.yaml up --build 
+# docker-compose -f docker-compose-dev.yaml up --build --scale raw_data_receiver=4 
+docker-compose -f docker-compose-dev.yaml up --build
